43f75de42abfe6400df17376ae1c374469c43e6e
Luís Tovar <1181585@isep.ipp.pt>
2021-03-01 19:27:00 +0000

    Melhorar Explicação nas MethodReferences

--- src/units/MethodReferences.java
+++ src/units/MethodReferences.java
@@ -2,41 +2,45 @@ package units;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.ObjDoubleConsumer;
 
 public class MethodReferences {
 
     public static void main(String[] args) {
-        System.out.println("\n### Exemplos Method References ###");
+        System.out.println("\n### Method References ###");
 
-        /* quando temos uma expressão lambda sem argumentos de input e que apenas chama um método sem nenhum argumento
-        (ou seja, a expressão lambda é apenas um pass-through), a expressão pode ser substituída por uma method reference
-        (Runnable é uma interface funcional para métodos sem input nem output) */
+        /* quando temos uma expressão lambda que apenas chama um método (ou seja, a expressão lambda é apenas um
+        pass-through), a expressão pode ser substituída por uma method reference. method references não têm vantagens
+        significativas, mas é bom sabermos que existem. basicamente fazem o código mais bonito
+        a sintaxe consiste em: <objeto ou classe sobre o qual se executa o método> :: <nome do método> */
+
+        // neste caso, chama-se um método que não recebe nenhum input nem retorna nenhum output
         Runnable callPrintSomething = () -> printSomething();
         // esta expressão lambda pode ser substituída por:
         callPrintSomething = MethodReferences::printSomething;
         callPrintSomething.run();
-        // a sintaxe consiste em: <objeto ou classe sobre o qual se executa o método> :: <nome do método>
-        // method references não têm vantagens significativas, mas é bom sabermos que existem. só fazem o código mais bonito
 
-        /* também é válido usar method references sempre que se tem uma expressão lambda que recebe argumentos, mas o
-        conteúdo do lambda apenas envia esses parâmetros como parâmetro de uma função chamada. novamente, quando a
-        expressão lambda é apenas um pass-through */
+        // neste caso, chama-se um método que recebe input mas não retorna output
+        // o objeto sobre o qual é chamado o método desejado é System.out, uma variável global pública da classe System
+        Consumer<String> printString = (String s) -> System.out.println(s);
+        printString = System.out::println;
+        printString.accept("A consumer is printing this through a method reference of System.out.println");
+        // outro exemplo com input sem output
         ObjDoubleConsumer<ArrayList<Double>> multiplyAllElements = (ArrayList<Double> arraylist, double dValue) -> multiplyArrayElements(arraylist, dValue);
         // pode ser substituído por
         multiplyAllElements = MethodReferences::multiplyArrayElements;
+        // vamos testá-lo
         ArrayList<Double> doubleArrayList = new ArrayList<>();
         doubleArrayList.add(4.6);
         doubleArrayList.add(5.123);
         doubleArrayList.add(0.543451);
         doubleArrayList.add(20123.476123);
         multiplyAllElements.accept(doubleArrayList, 0.8);
-        // outro exemplo
-        // o objeto sobre o qual é chamado o método desejado é System.out, uma variável global pública da classe System
-        Consumer<String> printString = System.out::println;
-        printString.accept("A consumer is printing this through a method reference of System.out.println");
 
-        // Essencialmente, podem ser usadas method references sempre que não há output na expressão lambda que escrevemos
+        // neste caso há input e output no método chamado
+        Function<ArrayList<String>, String> getLastElement = (ArrayList<String> arrayList) -> lastElement(arrayList);
+        getLastElement = MethodReferences::lastElement;
 
     }
 
@@ -46,6 +50,10 @@ public class MethodReferences {
 
     public static void multiplyArrayElements(ArrayList<Double> arr, double d) {
         arr.forEach((Double element) -> System.out.println(element * d));
+    }
+
+    private static String lastElement(ArrayList<String> strings) {
+        return strings.get(strings.size() - 1);
     }
 
 }
